bf0b687452bb137ca3a4b274b6555df723fa5f37
Jgier47 <60177897+Jgier47@users.noreply.github.com>
2024-06-27 10:58:24 +0200

    Swagger documentation added (#2)

--- src/main/java/net/java/app/organizationservice/dto/OrganizationDto.java
+++ src/main/java/net/java/app/organizationservice/dto/OrganizationDto.java
@@ -1,10 +1,12 @@
 package net.java.app.organizationservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "Organization DTO Model Information")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,11 +14,15 @@ public class OrganizationDto {
 
   private Long id;
 
+  @Schema(description = "Organization Name")
   private String organizationName;
 
+  @Schema(description = "Organization Description")
   private String organizationDescription;
 
+  @Schema(description = "Organization Code")
   private String organizationCode;
 
+  @Schema(description = "Organization creation date")
   private LocalDateTime createdDate;
 }
